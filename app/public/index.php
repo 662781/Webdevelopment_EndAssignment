53c0406e5365cc2f554de3fc348e08a9deb790d9
662781 <662781@student.inholland.nl>
2021-11-24 16:30:13 +0100

    Progress End Assignment

--- app/public/index.php
+++ app/public/index.php
@@ -1,16 +1,57 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <link rel="icon" href="images/pizza_logo.png" type="image" sizes="16x16">
+    <title>Pizza Time - Home</title>
 </head>
-<body>
-    
-</body>
-</html>
 
-<?php
-echo "Hello World!";
-?>
+<body>
+    <div class="container mt-2">
+        <div class="row">
+            <div class="col-md">
+                <nav class="py-2 bg-light border-bottom">
+                    <div class="container d-flex flex-wrap">
+                        <ul class="nav me-auto">
+                            <li class="nav-item"><a href="#" class="nav-link link-dark px-2 active" aria-current="page" >Home</a></li>
+                            <li class="nav-item"><a href="#" class="nav-link link-dark px-2">Pizza</a></li>
+                            <li class="nav-item"><a href="#" class="nav-link link-dark px-2">Pasta</a></li>
+                            <li class="nav-item"><a href="#" class="nav-link link-dark px-2">About</a></li>  
+                        </ul>
+                        <ul class="nav">
+                            <li class="nav-item"><a href="#" class="nav-link link-dark px-2">Login</a></li>
+                            <li class="nav-item"><a href="#" class="nav-link link-dark px-2">Sign up</a></li>
+                            <li class="nav-item"><a href="#" class="nav-link link-dark px-2">Cart</a></li>
+                        </ul>
+                    </div>
+                </nav>
+            </div>
+        </div>
+        <div class="row">
+            <header class="py-3 mb-4 border-bottom">
+                <div class="container d-flex flex-wrap justify-content-center">
+                    <a href="/" class="d-flex align-items-center mb-3 mb-lg-0 me-lg-auto text-dark text-decoration-none">
+                        <img class= "logo" src="images/pizza_logo.png" alt="Pizza Time!">                       
+                    </a>
+                </div>
+            </header>
+        </div>
+        <div class="row">
+            <div class="col-md-4">
+                Something here
+            </div>
+            <div class="col-md-8">
+                Something there
+            </div>
+        </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+</body>
+
+</html>
