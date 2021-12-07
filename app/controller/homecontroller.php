7f22728db7690cddf2f6ce76858c1297a54472f8
662781 <70585970+662781@users.noreply.github.com>
2021-12-07 12:33:59 +0100

    Update homecontroller.php

--- app/controller/homecontroller.php
+++ app/controller/homecontroller.php
@@ -5,6 +5,10 @@ class HomeController{
         echo "You've reached the index method of the home controller";
     }
 
+    public function home(){
+        include_once "/../view/home/home.php";
+    }
+
     public function about(){
         echo "You've reached the about method of the home controller";
     }
