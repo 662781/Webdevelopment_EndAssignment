ff7d0ef102bf1f5830c31acbb2e34dc5505e94c5
662781 <662781@student.inholland.nl>
2022-01-24 11:13:22 +0100

    Update index.php

--- app/public/index.php
+++ app/public/index.php
@@ -1,7 +1,5 @@
-<!-- Dit is het enige wat er in dit bestand moet (aangezien de link naar hier in het nginx bestand staat). 
-Zo hoef je niet meer perse de index als 1e pagina te gebruiken en staat je URL niet meer vol met .php dingen. 
-Ook gebruik je 1 router class. De PatternRouter is de handigste en meest dynamische.-->
 <?php
+    error_reporting(E_ALL);
     // require __DIR__ . '/../switchrouter.php';
     require __DIR__ . '/../patternrouter.php';
 
